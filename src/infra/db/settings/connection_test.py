20f4cfdbe96b3b89290b6df8c81610455f29465f
Bruno Costa <bruno.costa105@hotmail.com>
2024-02-10 03:03:52 -0300

    🗃️ create database connection

---
+++ src/infra/db/settings/connection_test.py
@@ -0,0 +1,10 @@
+import pytest
+from src.infra.db.settings.connection import DBConnectionHandler
+
+
+@pytest.mark.skip(reason="Sensive test")
+def test_create_database_engine():
+    db_connection_handler = DBConnectionHandler()
+    engine = db_connection_handler.get_engine()
+
+    assert engine is not None
